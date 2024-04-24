3efeccd173d565a19b7c95d61e15fe2ade636101
alfian-f <alfian.0615@gmail.com>
2024-04-24 18:13:26 +0700

    Simulation slow subscriber

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ impl MessageHandler<UserCreatedEventMessage> for UserCreatedHandler {
         let ten_millis = time::Duration::from_millis(1000);
         let now = time::Instant::now();
 
-        // thread::sleep(ten_millis);
+        thread::sleep(ten_millis);
         println!("In Alfian's Computer [2206821683]. Message received: {:?}", message);
         Ok(())
     }
